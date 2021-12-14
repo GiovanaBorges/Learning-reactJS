5b5642aa0f19397708a81cc3eadd1b67322b5519
giovana borges <gigi12345as17@gmail.com>
2021-12-13 21:14:31 -0300

    change the bg

--- aniflix/src/assets/pages/Home/index.jsx
+++ aniflix/src/assets/pages/Home/index.jsx
@@ -7,35 +7,54 @@ const Home = () => {
     <>
       <Navbar />
       <HomeWrapper>
-        <h1>COMO DIMINUI?::</h1>
+        <h1>Quem está Assistindo ?</h1>
         <BoxContent>
           <div>
-            <img
-              src="https://exame.com/wp-content/uploads/2016/09/size_960_16_9_foto-de-perfil-da-netflix.jpg"
-              alt="perfil fedido"
-            />
-            <h1>mirellão</h1>
+            <div className="img_container">
+              <img
+                src="https://meupet.elanco.com/sites/g/files/adhwdz661/files/styles/paragraph_image/public/2020-07/gato_malhado_azul_maine_cool_no_quintal_rodeado_por_flores_amarelas_no_gramado.jpg?itok=fUTBtgXC"
+                alt="perfil fedido"
+              />
+            </div>
+            <div>
+              <h1>Sandra</h1>
+            </div>
           </div>
+
           <div>
-            <img
-              src="https://exame.com/wp-content/uploads/2016/09/size_960_16_9_foto-de-perfil-da-netflix.jpg"
-              alt="perfil fedido"
-            />
-            <h1>jorgitoLITO</h1>
+            <div className="img_container">
+              <img
+                src="https://meupet.elanco.com/sites/g/files/adhwdz661/files/styles/paragraph_image/public/2020-07/gato_malhado_azul_maine_cool_no_quintal_rodeado_por_flores_amarelas_no_gramado.jpg?itok=fUTBtgXC"
+                alt="perfil fedido"
+              />
+            </div>
+            <div>
+              <h1>Sandra</h1>
+            </div>
           </div>
+
           <div>
-            <img
-              src="https://img.ifunny.co/images/b2dc95e364500a6d52d302cc37dedb52d9cdeab683be8e1372ec3c2dee2d6bee_1.jpg"
-              alt="perfil fedido"
-            />
-            <h1>Sabrinuda</h1>
+            <div className="img_container">
+              <img
+                src="https://meupet.elanco.com/sites/g/files/adhwdz661/files/styles/paragraph_image/public/2020-07/gato_malhado_azul_maine_cool_no_quintal_rodeado_por_flores_amarelas_no_gramado.jpg?itok=fUTBtgXC"
+                alt="perfil fedido"
+              />
+            </div>
+            <div>
+              <h1>Sandra</h1>
+            </div>
           </div>
+
           <div>
-            <img
-              src="https://exame.com/wp-content/uploads/2016/09/size_960_16_9_foto-de-perfil-da-netflix.jpg"
-              alt="perfil fedido"
-            />
-            <h1>Pairulito</h1>
+            <div className="img_container">
+              <img
+                src="https://meupet.elanco.com/sites/g/files/adhwdz661/files/styles/paragraph_image/public/2020-07/gato_malhado_azul_maine_cool_no_quintal_rodeado_por_flores_amarelas_no_gramado.jpg?itok=fUTBtgXC"
+                alt="perfil fedido"
+              />
+            </div>
+            <div>
+              <h1>Sandra</h1>
+            </div>
           </div>
         </BoxContent>
         <Button text={"GERENCIAR PERFIS"} />
