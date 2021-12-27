3b808f54b7293019e7632f036fc47dd37347a27a
giovana borges <gigi12345as17@gmail.com>
2021-12-27 18:04:58 -0300

    upgrade axios path

--- pokemonApi/src/App.js
+++ pokemonApi/src/App.js
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Api } from "./services/api";
+import style from "./style.css";
 
 function App() {
   const { register, handleSubmit } = useForm();
-  const [pokemon, setPokemon] = useState();
-  const [pokemonImg, setPokemonImg] = useState();
-  const [height, setHeight] = useState();
-  const [weight, setWeight] = useState();
-  const [baseExperience, setbaseExperience] = useState();
+  const [pokemon, setPokemon] = useState("pikachu");
+  const [pokemonImg, setPokemonImg] = useState(
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+  );
+  const [height, setHeight] = useState("Height : 4");
+  const [weight, setWeight] = useState("Weight : 60");
+  const [baseExperience, setbaseExperience] = useState("Experience : 112");
+  const [error, setError] = useState(false);
 
   function getPokemon(poke) {
     try {
-      Api.get(`/${poke}`).then((response) => {
+      Api.get(`/pokemon/${poke}`).then((response) => {
         console.log(response.data);
         setPokemonInformations(response.data);
       });
@@ -22,9 +26,12 @@ function App() {
   }
 
   function setPokemonInformations(informations) {
-    setPokemon(informations.name);
+    setPokemon(
+      informations.name[0].toUpperCase() + informations.name.substr(1)
+    );
+
     setWeight("Weight : " + informations.weight);
-    setHeight("Height :" + informations.height);
+    setHeight("Height : " + informations.height);
     setPokemonImg(informations.sprites.front_default);
     setbaseExperience("Experience : " + informations.base_experience);
   }
@@ -36,16 +43,37 @@ function App() {
 
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label>digite o name do coisa</label>
-        <input {...register("namePokemon")} />
+      <form onSubmit={handleSubmit(onSubmit)} style={style}>
+        <div className="search" style={style}>
+          <input
+            className="searchInput"
+            {...register("namePokemon")}
+            placeholder={"Pikachu"}
+          />
+          <input className="searchBtn" type="submit" value="Pesquisar" />
+        </div>
 
-        <input type="submit" value="submit" />
-        <h1>{pokemon}</h1>
-        <img src={pokemonImg} alt="pokemon" />
-        <h1>{height}</h1>
-        <h1>{weight}</h1>
-        <h1>{baseExperience}</h1>
+        {error ? (
+          <div className="error" style={style}>
+            <h1>
+              Pokemon Não encontrado , Verifique se digitou direito {pokemon}
+            </h1>
+          </div>
+        ) : (
+          <>
+            <div className="card">
+              <div style={style}>
+                <h1 className="pokemonName">{pokemon}</h1>
+                <h1 className="pokemonHeight">{height}</h1>
+                <h1 className="pokemonWeight"> {weight}</h1>
+                <h1 className="pokemonExperience">{baseExperience}</h1>
+              </div>
+              <div className="imgField">
+                <img src={pokemonImg} alt="pokemon" />
+              </div>
+            </div>
+          </>
+        )}
       </form>
     </>
   );
